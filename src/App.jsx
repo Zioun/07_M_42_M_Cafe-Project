ca741f6b93718c69a279606d63c84e5dc8860ace
Zioun <jionkhan0@gmail.com>
2024-03-14 01:23:44 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-slate-600 text-white text-center">Hello world!</h1>
+      <h1 className="text-2xl bg-slate-600 text-white text-center">Hello world</h1>
     </>
   )
 }
